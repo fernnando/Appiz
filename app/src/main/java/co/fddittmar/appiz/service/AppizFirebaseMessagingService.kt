d5d1063062d612b302497b6238f0ee4cfda4f8c3
fernnando <fddittmar@gmail.com>
2018-06-08 02:34:20 -0300

    - Getting Push notification.

---
+++ app/src/main/java/co/fddittmar/appiz/service/AppizFirebaseMessagingService.kt
@@ -0,0 +1,46 @@
+package co.fddittmar.appiz.service
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.support.v4.app.NotificationCompat
+import co.fddittmar.appiz.MainActivity
+import co.fddittmar.appiz.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import org.json.JSONException
+
+class AppizFirebaseMessagingService: FirebaseMessagingService() {
+
+    internal var intent: Intent? = null
+
+    override fun onMessageReceived(message: RemoteMessage?) {
+        super.onMessageReceived(message)
+        println(message)
+
+        send(message)
+    }
+
+    @Throws(JSONException::class)
+    internal fun send(remoteMessage: RemoteMessage?) {
+        intent = Intent(this, MainActivity::class.java)
+        intent!!.putExtra("title",remoteMessage!!.notification?.title)
+        intent!!.putExtra("body", remoteMessage.notification?.body)
+        val inboxStyle = NotificationCompat.InboxStyle()
+        val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        intent!!.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val notificationBuilder = NotificationCompat.Builder(this)
+        notificationBuilder.setSmallIcon(R.mipmap.ic_launcher)
+        notificationBuilder.setSound(uri)
+        notificationBuilder.setStyle(inboxStyle)
+        notificationBuilder.setContentText(remoteMessage.notification?.body)
+        notificationBuilder.setContentTitle(remoteMessage.notification?.title)
+        notificationBuilder.setAutoCancel(true)
+        notificationBuilder.setContentIntent(pendingIntent)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(System.currentTimeMillis().toInt(), notificationBuilder.build())
+    }
+}
